8ffd5180048c9733d3ee14bab85c3a089b3d883b
Bartosz Pniewski <pniewski.b@o2.pl>
2021-01-16 20:36:57 +0100

    App Architecture Communicating the Items Prop Building and Styling the Accordion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a fron end javascript framework'
+  },
+
+  {
+    title: 'Why use React?',
+    content: 'React is favorite JS libary among engineers'
+  },
+
+  {
+    title: 'How to use React?',
+    content: 'You use Ract by creating components'
+  }
+];
 
 export default () => {
-  return <h1>widgets</h1>;
+  return <div>
+          <Accordion items={items} />
+        </div>
 };

20d6e4f71c0267ecec1ad187e6105c902f212aea
pnievsky <pniewski.b@o2.pl>
2021-02-19 20:02:33 +0100

    Changing the URL Detecting Navigation Updating the Route Handling Command Clicks

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
+
     event.preventDefault();
+    /*changing url without reloading a webpage*/
+    window.history.pushState({}, '', href);
+    /*event for changing url*/
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
   };
 
   return (
